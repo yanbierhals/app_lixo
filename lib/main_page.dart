da8f467bb23f2da946dc6f08a375e2ad18e6ce61
Yan Bierhals <yanrbierhals@gmail.com>
2024-11-30 08:19:41 -0300

    reabrir cam

--- lib/main_page.dart
+++ lib/main_page.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:mobile_scanner/mobile_scanner.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
-import 'dart:async';  // Importa a biblioteca para controle de tempo
+import 'dart:async';
 
 class MainPage extends StatefulWidget {
   const MainPage({super.key});
@@ -13,44 +13,35 @@ class MainPage extends StatefulWidget {
 
 class _MainPageState extends State<MainPage> {
   String qrCodeData = "Nenhum código lido";
-  bool isPostSent = false;  // Variável para controlar se o POST já foi enviado
-  bool isScanning = true;    // Variável para controlar se o scanner está ativo
-  bool isSending = false;    // Variável para controlar se a requisição POST está sendo enviada
+  bool isPostSent = false;
+  bool isScanning = true;
+  bool isSending = false;
 
-  // Método para enviar os dados via POST
   Future<void> sendQRCodeData(String idLixeira) async {
-    // Impede múltiplas requisições rápidas
-    if (isSending) return; // Verifica se já está enviando, se sim, não faz mais nada
+    if (isSending) return;
 
     setState(() {
-      isSending = true;  // Inicia o envio da requisição
+      isSending = true;
     });
 
     final url = Uri.parse('https://api-lixo.onrender.com/api/leituras/inserir');
-
-    // Corpo da requisição
     final body = jsonEncode({
-      "id_usuario": 12, // Exemplo de id_usuario, pode ser alterado conforme necessário
+      "id_usuario": 12,
       "id_lixeira": idLixeira,
     });
-
-    // Cabeçalhos da requisição
     final headers = {
       "Content-Type": "application/json",
     };
 
     try {
-      // Enviando a requisição POST
       final response = await http.post(
         url,
         headers: headers,
         body: body,
       );
 
-      // Verifica se a requisição foi bem-sucedida
       if (response.statusCode == 200 || response.statusCode == 201) {
         print('Dados enviados com sucesso!');
-        // Exibe a notificação de sucesso
         showSuccessNotification();
       } else {
         print('Erro ao enviar dados: ${response.statusCode}');
@@ -58,22 +49,19 @@ class _MainPageState extends State<MainPage> {
     } catch (e) {
       print('Erro: $e');
     } finally {
-      // Após 2 segundos, permite uma nova leitura
-      await Future.delayed(Duration(seconds: 2)); // Intervalo de 2 segundos
+      await Future.delayed(Duration(seconds: 2));
       setState(() {
-        isSending = false;  // Permite enviar outra requisição
+        isSending = false;
       });
     }
   }
 
-  // Método para exibir a notificação de sucesso
   void showSuccessNotification() {
     setState(() {
-      isPostSent = true;  // Marca que o POST foi enviado
-      isScanning = false; // Desativa o scanner após a leitura
+      isPostSent = true;
+      isScanning = false;
     });
 
-    // Exibe a notificação de sucesso
     ScaffoldMessenger.of(context).showSnackBar(
       SnackBar(content: Text('Lixeira lida com sucesso!')),
     );
@@ -108,29 +96,27 @@ class _MainPageState extends State<MainPage> {
         ),
       ),
       body: Center(
-        child: Column(
+        child: isScanning
+            ? Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            // Exibe o scanner de QR Code apenas se o POST ainda não foi enviado
-            if (isScanning)
-              SizedBox(
-                width: 300,
-                height: 300,
-                child: MobileScanner(
-                  onDetect: (BarcodeCapture barcodeCapture) {
-                    if (!isPostSent && !isSending) {
-                      final barcode = barcodeCapture.barcodes.first; // Obtém o primeiro código de barras detectado
-                      if (barcode.rawValue != null) {
-                        setState(() {
-                          qrCodeData = barcode.rawValue!;
-                        });
-                        // Chama o método para enviar os dados via POST
-                        sendQRCodeData(qrCodeData);
-                      }
+            SizedBox(
+              width: 300,
+              height: 300,
+              child: MobileScanner(
+                onDetect: (BarcodeCapture barcodeCapture) {
+                  if (!isPostSent && !isSending) {
+                    final barcode = barcodeCapture.barcodes.first;
+                    if (barcode.rawValue != null) {
+                      setState(() {
+                        qrCodeData = barcode.rawValue!;
+                      });
+                      sendQRCodeData(qrCodeData);
                     }
-                  },
-                ),
+                  }
+                },
               ),
+            ),
             SizedBox(height: 20),
             Text(
               'Conteúdo do QR Code:',
@@ -140,6 +126,26 @@ class _MainPageState extends State<MainPage> {
             Text(
               qrCodeData,
               style: TextStyle(fontSize: 16),
+            ),
+          ],
+        )
+            : Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Scanner inativo',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  isScanning = true;
+                  isPostSent = false;
+                  qrCodeData = "Nenhum código lido";
+                });
+              },
+              child: Text('Reabrir Scanner'),
             ),
           ],
         ),
